51f7362b5811e412889d1c8b4fcbd205c5af1ecc
Sheetal Gat <105741952+sheetalgat-hubGit@users.noreply.github.com>
2026-01-25 13:29:04 +0530

    data ingestion added

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -2,7 +2,7 @@ import os
 import zipfile
 import gdown
 from cnnClassifier import logger
-# from cnnClassifier.utils.common import get_size
+from cnnClassifier.utils.common import get_size
 from cnnClassifier.entity.config_entity import DataIngestionConfig
 
 
